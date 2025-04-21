f74c59939087cc19dfb8e0c8bc0bea61fd7ce37c
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2025-04-22 01:08:14 +0530

    insert into queue, check unvisited neighbours from adj list, keep doing till q is empty
    https://takeuforward.org/graph/breadth-first-search-bfs-level-order-traversal/

---
+++ Graphs/BFS.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+class Solution {
+    // Function to return Breadth First Search Traversal of given graph.
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        int v=adj.size();
+        boolean[] vis=new boolean[v];
+        vis[0]=true;
+        Queue<Integer> q=new LinkedList<>();
+        q.add(0);
+        while(!q.isEmpty()){
+            int node=q.poll();
+            ans.add(node);
+            for(int i:adj.get(node)){
+                if(!vis[i]){
+                    vis[i]=true;
+                    q.add(i);
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                String[] input = br.readLine().trim().split(" ");
+                ArrayList<Integer> node = new ArrayList<>();
+                for (String s : input) {
+                    if (!s.isEmpty()) {
+                        node.add(Integer.parseInt(s));
+                    }
+                }
+                adj.add(node);
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfs(adj);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
