69a576467e73cc9c8049e16f989237edc4b66d44
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2025-04-22 16:36:30 +0530

    mark node visited and add in ans, then recursive call for each of unvisisted neighbours in adj list,
    https://takeuforward.org/data-structure/depth-first-search-dfs/

---
+++ Graphs/DFS.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                String[] input = br.readLine().trim().split(" ");
+                ArrayList<Integer> node = new ArrayList<>();
+                for (String s : input) {
+                    if (!s.isEmpty()) {
+                        node.add(Integer.parseInt(s));
+                    }
+                }
+                adj.add(node);
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfs(adj);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        int v=adj.size();
+        boolean[] vis=new boolean[v];
+        vis[0]=true;
+        ArrayList<Integer> ans=new ArrayList<>();
+        dfs(0,vis,adj,ans);
+        return ans;
+    }
+    private void dfs(int node,boolean[] vis,ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> ans){
+        vis[node]=true;
+        ans.add(node);
+        for(int i:adj.get(node)){
+            if(!vis[i]){
+                dfs(i,vis,adj,ans);
+            }
+        }
+    }
+}
